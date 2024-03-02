c3eff92ff1b1026549102ecd68ef707cea0d06e8
hfad0002 <hfad0002@student.monash.edu>
2024-03-02 21:38:53 +1100

    add routing
    add routing for player and code room generator

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
 import random
 import string
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
-# Dictionary to store rooms and their corresponding codes
+# Dictionary to store rooms
 rooms = {}
 
 # Function to generate a random 6-digit code
@@ -12,23 +13,40 @@ def generate_room_code():
     while True:
         room_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
         if room_code not in rooms:
-            rooms.add(room_code)
             return room_code
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/create_room')
+@app.route('/create_room', methods=['POST'])
 def create_room():
+    player_name = request.form['player_name']
     room_code = generate_room_code()
-    rooms[room_code] = []
+    session['player_name'] = player_name
+    rooms[room_code] = [player_name]
+    return redirect(url_for('room', room_code=room_code))
+
+@app.route('/join_room', methods=['POST'])
+def join_room():
+    room_code = request.form['room_code']
+    player_name = request.form['player_name']
+
+    session['player_name'] = player_name
+    
+    if room_code in rooms:
+        rooms[room_code].append(player_name)
+    else:
+        rooms[room_code] = [player_name]
+    
     return redirect(url_for('room', room_code=room_code))
 
 @app.route('/room/<string:room_code>')
 def room(room_code):
     if room_code in rooms:
-        return render_template('room.html', room_code=room_code)
+        players = rooms[room_code]
+        current_player = session.get('player_name')
+        return render_template('room.html', room_code=room_code, players=players, current_player=current_player)
     else:
         return redirect(url_for('index'))
 
